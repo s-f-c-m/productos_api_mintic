ee1a8613bc8eac0c4ada8caca4362a7fd0f43513
Sebastian Castano <sebastian9111@gmail.com>
2021-12-11 22:42:24 +0200

    add jwt not valid exception

--- src/main/java/com/tiendagenerica/productos/ProductoController.java
+++ src/main/java/com/tiendagenerica/productos/ProductoController.java
@@ -3,6 +3,8 @@ package com.tiendagenerica.productos;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.security.auth.message.AuthException;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -22,25 +24,29 @@ public class ProductoController {
 
 
     @PostMapping
-    public void insertProducto(@RequestHeader Map<String, String> headers, @RequestBody List<Producto> listProducto) {
+    public void insertProducto(@RequestHeader Map<String, String> headers, @RequestBody List<Producto> listProducto) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             productoService.insertProducto(listProducto);
+        }else{
+            throw new AuthException("No authorizado");
         }
     }
 
     @GetMapping()
-    public List<Producto> getAllProductos(@RequestHeader Map<String, String> headers) {
+    public List<Producto> getAllProductos(@RequestHeader Map<String, String> headers) throws Exception {
         if(jwtchecker.doFilter(headers)){
             return productoService.getAll();
+        }else{
+            throw new AuthException("No authorizado");
         }
-        return null ;
     }
     @GetMapping("/{codigoProducto}")
-    public Optional<Producto> getByCodigoProducto(@RequestHeader Map<String, String> headers, @PathVariable String codigoProducto){
+    public Optional<Producto> getByCodigoProducto(@RequestHeader Map<String, String> headers, @PathVariable String codigoProducto) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             return productoService.getByCodigoProducto(codigoProducto);
-        }
-        return null ;
+    }else{
+        throw new AuthException("No authorizado");
+    }
 //        System.out.println(temp);
 //        return temp;
     }

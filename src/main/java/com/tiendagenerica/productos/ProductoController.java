c82ed1a57a78bedc1838ab12f594b259110e6b70
Sebastian Castano <sebastian9111@gmail.com>
2021-12-11 22:01:53 +0200

    add jwt checker

--- src/main/java/com/tiendagenerica/productos/ProductoController.java
+++ src/main/java/com/tiendagenerica/productos/ProductoController.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.resolve;
 
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
@@ -13,21 +17,30 @@ import java.util.Optional;
 public class ProductoController {
 
     private final ProductoService productoService;
+    private final JWTChecker jwtchecker;
+
 
 
     @PostMapping
-    public void insertProducto(@RequestBody List<Producto> listProducto) {
-        productoService.insertProducto(listProducto);
-
+    public void insertProducto(@RequestHeader Map<String, String> headers, @RequestBody List<Producto> listProducto) {
+        if(jwtchecker.doFilter(headers)){
+            productoService.insertProducto(listProducto);
+        }
     }
 
     @GetMapping()
-    public List<Producto> getAllProductos() {
-        return productoService.getAll();
+    public List<Producto> getAllProductos(@RequestHeader Map<String, String> headers) {
+        if(jwtchecker.doFilter(headers)){
+            return productoService.getAll();
+        }
+        return null ;
     }
     @GetMapping("/{codigoProducto}")
-    public Optional<Producto> getByCodigoProducto(@PathVariable String codigoProducto){
-       return productoService.getByCodigoProducto(codigoProducto);
+    public Optional<Producto> getByCodigoProducto(@RequestHeader Map<String, String> headers, @PathVariable String codigoProducto){
+        if(jwtchecker.doFilter(headers)){
+            return productoService.getByCodigoProducto(codigoProducto);
+        }
+        return null ;
 //        System.out.println(temp);
 //        return temp;
     }
